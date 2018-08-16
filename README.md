c6110d255e35f40ee176a9cba11c2fa4509d14c3
yuanliwei <891789592@qq.com>
2018-08-17 00:09:07 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # web-loader
-web resource loader tool
+load javascript or css files in browser by javascript
+
+## load javascript or css
+- basic use
+  ```javascript
+  const LoadES6 = require('web-loader');
+  new LoadES6()
+    .load("/src/js/jquery.js")
+    .load("/jquery-ui.min.css")
+    .wait(500)
+    .then(()=>console.log('init!'))
+    .load("/app.js")
+    .then(()=>console.log('over!'))
+  ```
+
+## use load with config
+- config.js
+  ```javascript
+  LoadES6.config({
+    "jquery":[
+      "https://cdn.bootcss.com/jquery/3.1.1/jquery.min.js"
+    ],
+    "jqueryui":[
+      "https://cdn.bootcss.com/jqueryui/1.12.1/jquery-ui.structure.min.css",
+      "https://cdn.bootcss.com/jqueryui/1.12.1/jquery-ui.theme.min.css",
+      "https://cdn.bootcss.com/jqueryui/1.12.1/jquery-ui.min.js",
+      "https://cdn.bootcss.com/jqueryui/1.12.1/jquery-ui.min.css"
+    ]})
+  ```
+- script
+  ```javascript
+  const LoadES6 = require('web-loader');
+  new LoadES6()
+    .load("/config.js")
+    .load("jquery", "jqueryui")
+    .wait(500)
+    .then(()=>console.log('init!'))
+    .load("/app.js")
+    .then(()=>console.log('over!'))
+  ```
---
+++ index.js
@@ -0,0 +1,130 @@
+module.exports = class LoadES6 {
+
+  constructor(promise) {
+    this.promise = promise || Promise.resolve()
+    return this;
+  }
+
+  load() {
+    var params = [...arguments];
+    this.promise = this.promise.then(()=>{
+      console.log("load:" + JSON.stringify(params));
+      var promises = LoadES6.getLoadPromises(params);
+      return Promise.all(promises).catch((e)=>console.error(e));
+    });
+    return new LoadES6(this.promise);
+  }
+
+  then(callback, isAsync) {
+    this.promise = this.promise.then((data)=>{
+      return new Promise((resolve, reject)=>{
+        if (isAsync) {
+          callback(resolve, data);
+        } else {
+          data = callback(data);
+          resolve(data);
+        }
+      }).catch((e)=>console.error(e));
+    });
+    return new LoadES6(this.promise);
+  }
+
+  wait(millis) {
+    return this.then((resolve)=>{ setTimeout(resolve, millis) }, true);
+  }
+
+  hide(selector) {
+    selector = selector || 'body';
+    return this.then(()=> { document.querySelector(selector).style.display='none' })
+  }
+
+  show(selector) {
+    selector = selector || 'body';
+    return this.then(()=> { document.querySelector(selector).style.display='' })
+  }
+
+  static config(config){
+    Object.assign(LoadES6.configuration, config)
+  }
+
+  static getLoadPromises(params) {
+    var urls = LoadES6.getUrls(params);
+    var promises = urls.map(url=>{
+      return new Promise((resolve, reject)=>{
+        var type = LoadES6.parseType(url);
+        switch (type) {
+          case "js":
+            LoadES6.loadScript(url, resolve);
+            break;
+          case "css":
+            LoadES6.loadLink(url, resolve);
+            break;
+          default:
+            console.error("unknow type! " + url);
+        }
+      });
+    });
+    return promises;
+  };
+
+  static loadScript(url, resolve) {
+    var node = document.createElement('script');
+    node.type = 'text/javascript';
+    node.charset = 'utf-8';
+    node.async = true;
+    node.src = url;
+    LoadES6.appendNode(node, url, resolve);
+  };
+
+  static loadLink(url, resolve) {
+    var node = document.createElement('link');
+    node.rel = "stylesheet";
+    node.href = url;
+    LoadES6.appendNode(node, url, resolve);
+  };
+
+  static getUrls(params) {
+    var urls = [];
+    params.forEach((param)=> {
+      if (typeof param == "function") {
+        param()
+        return
+      }
+      var configUrls = LoadES6.configuration[param];
+      if (configUrls) {
+        urls = urls.concat(configUrls);
+      } else {
+        urls.push(param);
+      }
+    });
+    return urls;
+  }
+
+  static appendNode(node, url, resolve) {
+    if (LoadES6.loaded[url]) {
+      resolve();
+      return;
+    }
+    node.onload = function () {
+      LoadES6.loaded[url] = true;
+      resolve();
+    };
+    node.onerror = function (e) {
+      console.error(JSON.stringify(e));
+      resolve();
+    };
+    document.head.appendChild(node);
+  }
+
+  static parseType(url) {
+    var urlArr = url.split('?');
+    var urls = urlArr[0].split('.');
+    var end = urls[urls.length-1];
+    return end.toLowerCase();
+  }
+
+}
+
+LoadES6.loaded = {};
+
+LoadES6.configuration = {};
